c1ca3e7555b7a1662654551f7ff6ff76d8071ef2
Ruben Matias Tiscar <rmatiast@emeal.nttdata.com>
2023-03-29 15:45:02 +0200

    Initial commit

---
+++ src/main/java/com/example/retoinicialserver/RetoInicialServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.retoinicialserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoInicialServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoInicialServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/retoinicialserver/RetoInicialServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retoinicialserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoInicialServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
